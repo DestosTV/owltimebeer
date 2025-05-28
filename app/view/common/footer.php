070c15f923024e8a02cce77a97ba1d4b02b12e76
DestosTV <duchampmorgan91@gmail.com>
2025-05-28 16:48:30 +0200

    dd
    test

--- app/view/common/footer.php
+++ app/view/common/footer.php
@@ -1,6 +1,21 @@
 </main>
 <footer>
-    <h3>Footer</h3>
+    <aside class="gauche">
+        <ul>
+            <li><a href="">FAQ</a></li>
+            <li><a href="">Mentions légales</a></li>
+            <li class="prevention">L'abus d'alcool est dangereux pour la santé, à consommer avec modération.</li>
+        </ul>
+    </aside>
+    <aside class="droite">
+        <p>Nos réseaux sociaux: </p>
+        <ul>
+            <li><img id="youtube" src="social_15707749.png" alt="Icone Youtube"></li>
+            <li><img id="instagram" src="social_16033438.png" alt="Icone Instagram"></li>
+            <li><img id="tiktok" src="tik-tok_4782345.png" alt="Icone Tik Tok"></li>
+        </ul>
+    </aside>
 </footer>
 </body>
+
 </html>
