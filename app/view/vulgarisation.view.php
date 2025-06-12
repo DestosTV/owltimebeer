4ec4cd85f4f5c7cc7a577b9009fa267ceac63dff
Dust_X <ambrine.rebai@gmail.com>
2025-06-13 01:40:20 +0200

    modif css plusieurs pages

--- app/view/vulgarisation.view.php
+++ app/view/vulgarisation.view.php
@@ -6,7 +6,7 @@
     Pensée pour les amateurs de saveurs raffinées, Owl Time incarne l’élégance britannique avec un soupçon d’audace. Son identité visuelle, marquée par un hibou stylisé, reflète à la fois la sagesse, l’optimisme et l’éclat que nous voulons transmettre dans chaque gorgée. C’est une bière qui se déguste lentement, avec intention, dans un moment de partage ou de réflexion.
   </p>
 
-  <h3>Les coulisses de Owl Time!</h3>
+  <h3>Les coulisses de Owl Time !</h3>
   <div class="video-container">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/32q_8Q86XKk?si=6bWczy5SBE5zO-tu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
   </div>
