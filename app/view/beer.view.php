bc6c5a13a48a470998406ffa4e5a871c154a41c6
DestosTV <duchampmorgan91@gmail.com>
2025-06-12 23:45:54 +0200

    cds

--- app/view/beer.view.php
+++ app/view/beer.view.php
@@ -1,7 +1,7 @@
 <h1><?= htmlspecialchars($biere['name']) ?></h1>
 <div class="beer">
     <img src="<?= htmlspecialchars($biere['photo']) ?>" alt="<?= htmlspecialchars($biere['name']) ?>" />
-    <p>Catégorie : <?= htmlspecialchars($biere['categorie']) ?></p>
+    <p class="categorie">Catégorie : <?= htmlspecialchars($biere['categorie']) ?></p>
     <?php if (!empty($biere['description'])): ?>
         <p class="description"><?= htmlspecialchars($biere['description']) ?></p>
     <?php endif; ?>
